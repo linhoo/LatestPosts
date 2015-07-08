9e85ae800d20abbf7fe1b66eee43e732e838990c
terry <linhu.li2012@hotmail.com>
2015-07-08 20:21:15 +0800

    Change to code to structure

--- blog_recent.js
+++ blog_recent.js
@@ -5,27 +5,37 @@ var http = require("http");
 var fs = require("fs");
 
 http.createServer(function(req, res) {
-   if (req.url == "/") {
-       fs.readFile("./titles.json", function(err, data) {
-          if (err) {
-              console.error(err);
-              res.end("Server Error");
-          }  else {
-              var titles = JSON.parse(data.toString());
-
-              fs.readFile("./template.html", function(err, data) {
-                  if (err) {
-                      console.error(err);
-                      res.end("Server Error");
-                  } else {
-                      var templ = data.toString();
-
-                      var html = templ.replace("%", titles.join("</li><li>"));
-                      res.writeHead(200, {"Content-Type": "text/html"});
-                      res.end(html);
-                  }
-              });
-          }
-       });
-   }
+    getTitles(res);
 }).listen(8000, "127.0.0.1");
+
+function getTitles(res) {
+    fs.readFile("./titles.json", function(err, data) {
+       if (err) {
+           hadError(err, res);
+       } else {
+           getTemplate(JSON.parse(data.toString()), res);
+       }
+    });
+}
+
+function getTemplate(titles, res) {
+    fs.readFile("./template.html", function(err, data) {
+       if (err) {
+           hadError(err, res);
+       } else {
+           formatHtml(titles, data.toString(), res);
+       }
+    });
+}
+
+function formatHtml(titles, templ, res) {
+    var html = templ.replace("%", titles.join("</li><li>"));
+
+    res.writeHead(200, {"Content-Type" : "text/html"});
+    res.end(html);
+}
+
+function hadError(err, res) {
+    console.error(err);
+    res.end("Server Error");
+}
